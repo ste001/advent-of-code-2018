2b50cff799bb7dd95302a392bbba88679239ecbb
ste001 <asso99@hotmail.com>
2018-12-02 18:01:20 +0100

    Add Day 2 Part 2 solution and input

--- Day_2/day_2.rb
+++ Day_2/day_2.rb
@@ -25,4 +25,42 @@ def checksum input
   checksum
 end
 
+def almost_equal(a, b)
+  changes = 0
+  a.length.times do |i|
+    if a[i] != b[i]
+      changes += 1
+    end
+  end
+  (changes == 1) ? true : false
+end
+
+def same_letters(a, b)
+  same_letters = ""
+  a.length.times do |i|
+    if a[i] == b[i]
+      same_letters << a[i]
+    end
+  end
+  same_letters
+end
+
+def common_letters input
+  input_array = input.split("\n")
+  i = 0
+  j = 0
+  n = input_array.length
+  while (i < n)
+    while (j < n)
+      if (i != j && almost_equal(input_array[i], input_array[j]))
+        return same_letters(input_array[i], input_array[j])
+      end
+      j += 1
+    end
+    j = 0
+    i += 1
+  end
+end
+
 puts "Part one solution: #{checksum input}"
+puts "Part two solution: #{common_letters input}"
